6a6fa43674f3447c40ff83c85aa5e75bdd768327
Augusto Maldonado Yubrin <augustomaldonadoy@gmail.com>
2023-01-18 15:34:32 -0300

    probando mail

---
+++ database/models/Actor copy.js
@@ -0,0 +1,48 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Actor";
+    let cols = {
+        id: {
+        type: dataTypes.INTEGER,
+        primaryKey : true,
+        autoIncrement : true
+        },
+        created_at: {
+            type: dataTypes.DATE
+        },
+        updated_at: {
+            type: dataTypes.DATE
+        },
+        first_name: {
+            type: dataTypes.STRING(100)
+        },
+        last_name:{
+            type: dataTypes.STRING(100)
+        },
+        rating:{
+            type: dataTypes.DECIMAL(3,1)
+        },
+        favorite_movie_id: {
+            type: dataTypes.INTEGER(10)
+        }
+        };
+    let config = {
+        tableName: "actors",
+        timestamps: false
+    }
+    
+      
+    const Actor = sequelize.define(alias, cols, config)
+    
+
+    Actor.associate = function(models) {
+        Actor.belongsToMany(models.Movie, {
+        as: "movies",
+        through: "ActorMovie",
+        foreignKey: "actor_id",
+        otherKey: "movie_id",
+        timestamps: false
+    })
+    }
+
+    return Actor;
+    }
